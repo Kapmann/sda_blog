0cb71b2203bc9a551b74ec696362809cf788efe1
unknown <kapmann@gmail.com>
2023-11-19 11:03:23 +0200

    uuendatud

--- blog_root/urls.py
+++ blog_root/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from posts.admin import post_site
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', post_site.urls),
     path("accounts/", include("accounts.urls")),
     path('about/', include('about.urls')),
     path("", include("posts.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# admin.site.site_title = "Admin Panel"
+# admin.site.site_header = "MY: Admin Panel"
+# admin.site.index_title = "MY:"
--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
 from .models import Post
-
+import django.apps
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    login_template = 'admin/login.html'
+    list_display = ('title', 'create_at', 'update_at', 'author')
+    # fields = ('title', 'body')
+    fieldsets = (
+        ('Post Information', {
+            'fields': ('title', 'body',)
+        }),
+        ('Author Information', {
+            'fields': ('author',)
+        })
+    )
+
+
+class PostAdminSite(admin.AdminSite):
+    site_header = "MY: Admin Panel"
+
+
+post_site = PostAdminSite(name='PostAdmin')
+post_site.register(Post, PostAdmin)
+
+models = django.apps.apps.get_models()
+for model in models:
+    try:
+        post_site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
